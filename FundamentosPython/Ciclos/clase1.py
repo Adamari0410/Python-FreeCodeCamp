6fdc2e45dac2aa2f1e29900c114a1c1081dc01c7
Adamari0410 <paolaadamarimaytaquispe@gmail.com>
2026-09-08 12:02:14 +0000

    Ciclos dia 1

---
+++ FundamentosPython/Ciclos/clase1.py
@@ -0,0 +1,18 @@
+#El tipo de dato listas es una secuencia ordenada de elementos que pueden estar
+# commpuestyos por cadenas ,numeros o inlcuso otra listas.
+#Las listas son mutables y usan indexacion basada en cero, lo que significa que 
+#el primer elemento de la lista esta en el indice cero
+ciudades= ["Lima", "Arequipa", "Puno"]
+#Para accedder a un elemento de la lista ciudades, puede referenciar su numero de
+#indice en la secuencia. aqui tienes un ejemplo de como acceder al primer elemento
+#de la lista ciudad
+print(ciudades[0])
+#El indexado negativo se usa para acceder des el fiunal de la lsita
+print(ciudades[-1])
+
+#Otra forma de convertir en lista es 
+nombre='Luhana'
+print(list(nombre))
+#Importante un iterable es un tipo especial de objeto que puede ser recorrido
+#uno por uno
+
